725d32702b31dd486a418663950a0b0f13889b8e
sdj3959 <dj9099@naver.com>
2025-08-05 10:31:04 +0900

    UserRepository 인터페이스 추가 및 사용자 조회/검증 메서드 구현

---
+++ src/main/java/com/spring/toyproject/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.spring.toyproject.repository;
+
+import com.spring.toyproject.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     *  로그인할 때 사용자는 자신의 이름이나 이메일을 입력하지, 숫자 ID를 입력하지 않음
+     *  그 이름이나 이메일을 통해 회원정보를 조회해야 한다.
+     */
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+    // 중복확인
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
