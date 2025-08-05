70fd1a047e4a7798ffb9fe665a773fcf046a22c6
sdj3959 <dj9099@naver.com>
2025-08-05 09:42:31 +0900

    Initial commit

---
+++ src/main/java/com/spring/toyproject/SpringToyProjectApplication.java
@@ -0,0 +1,14 @@
+package com.spring.toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToyProjectApplication {
+	int a;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringToyProjectApplication.class, args);
+	}
+
+}
