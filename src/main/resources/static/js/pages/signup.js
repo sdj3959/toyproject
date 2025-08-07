ffafa3ab7953651c5609cda216e1c681473ea430
sdj3959 <dj9099@naver.com>
2025-08-07 11:27:01 +0900

    Signup.js 초기화 로직 개선 및 head.html 스크립트 모듈화 처리

--- src/main/resources/static/js/pages/signup.js
+++ src/main/resources/static/js/pages/signup.js
@@ -1,10 +1,42 @@
-
 // 회원가입 관련 함수들의 모음
 const SignupPage = () => {
 
-// 초기화 함수
+  // 상태 관리 객체
+  const state = {
+    $form: null,
+    $signUpBtn: null,
+    $usernameInput: null,
+    $emailInput: null,
+    $passwordInput: null,
+    $confirmPasswordInput: null,
+  };
+
+  // 폼 제출 이벤트
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('form이 제출됨!');
+  };
+
+  // 이벤트 걸기
+  const bindEvents = () => {
+    // 1. form 제출 이벤트
+    state.$form.addEventListener('submit', handleSubmit);
+  };
+
+  // 초기화 함수
   const init = () => {
     console.log('회원가입 JS가 로딩되었습니다.');
+
+    // 필요한 DOM들을 가져오기
+    state.$form = document.getElementById('signupForm');
+    state.$signupBtn = document.getElementById('signupBtn');
+    state.$usernameInput = document.getElementById('usernameInput');
+    state.$emailInput = document.getElementById('email');
+    state.$passwordInput = document.getElementById('password');
+    state.$confirmPasswordInput = document.getElementById('confirmPassword');
+
+    // 이벤트 바인딩
+    bindEvents();
   };
 
   return {
