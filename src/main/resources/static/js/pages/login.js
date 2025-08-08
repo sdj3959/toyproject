d7ac64dcd865a5be6d4308e4d1b689e35823c78a
sdj3959 <dj9099@naver.com>
2025-08-08 10:38:51 +0900

    로그인 페이지 폼 이벤트 및 상태 관리 기능 추가 - `login.js`에 로그인 폼 상태 관리 객체(state) 추가 - 폼 제출 이벤트 핸들러(handleLogin) 및 서버 통신 로직 구현   - 로그인 API 요청 및 응답에 따라 결과 메시지 출력 - 폼 이벤트 바인딩(bindEvents) 로직 및 상태 초기화(init) 로직 구현 - 공통 유틸리티(api.js, util.js) 활용하여 로직 간소화 및 메시지 렌더링 처리

--- src/main/resources/static/js/pages/login.js
+++ src/main/resources/static/js/pages/login.js
@@ -1,10 +1,45 @@
+import { apiService } from '../utils/api.js';
+import { utils } from '../utils/util.js';
+
 
 // 로그인 관련 함수들의 모음
 const LoginPage = () => {
 
+  const state = {
+    $form: null,
+  };
+
+  // 로그인 이벤트
+  const handleLogin = async e => {
+    e.preventDefault();
+
+    // 폼 입력값 읽기
+    const formData = new FormData(state.$form);
+    const payload = Object.fromEntries(formData.entries());
+
+    /*const payload = {
+      usernameOrEmail : document.getElementById('usernameOrEmail').value,
+      password: document.getElementById('password').value
+    };*/
+
+    try {
+      const response = await apiService.post('/api/auth/login', payload);
+      console.log(response);
+      utils.showMessage(response.message, 'success');
+    } catch (error) {
+      utils.showMessage(error.message, 'error');
+    }
+  };
+
+  // 이벤트 걸기
+  const bindEvents = () => {
+    state.$form?.addEventListener('submit', handleLogin);
+  };
+
   // 초기화 함수
   const init = () => {
-    console.log('로그인 JS가 로딩되었습니다.');
+    state.$form = document.getElementById('loginForm');
+    bindEvents();
   };
 
   // public 함수
