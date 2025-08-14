73a183f87b5685475638aadaedb48d59418b6f94
sdj3959 <dj9099@naver.com>
2025-08-14 11:24:29 +0900

    여행일지 컨트롤러 추가 및 여행일지 생성 API 구현 - **TravelLogController**: 여행일지 관련 API 제공   - 여행일지 생성 API(`POST /api/travel-logs`) 추가   - MultipartFile 기반 파일 업로드 처리   - 사용자 인증 정보(`@AuthenticationPrincipal`) 활용 - **기능 설명**:   - 여행 ID 및 요청 데이터(TravelLogRequestDto) 유효성 검증   - 첨부 파일 저장 및 서비스 호출   - 성공 메시지 응답 반환

---
+++ src/main/java/com/spring/toyproject/api/TravelLogController.java
@@ -0,0 +1,45 @@
+package com.spring.toyproject.api;
+
+import com.spring.toyproject.domain.dto.common.ApiResponse;
+import com.spring.toyproject.domain.dto.request.TravelLogRequestDto;
+import com.spring.toyproject.service.TravelLogService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/travel-logs")
+public class TravelLogController {
+
+    private final TravelLogService travelLogService;
+
+    /**
+     * 여행일지 생성 API
+     * POST /api/travel-logs
+     *
+     * consumes : 클라이언트가 보낸 데이터의 형태 (기본값 : json)
+     * produces : 서버가 응답할 때 보내는 데이터 (기본값 : json)
+     */
+    @PostMapping
+    public ResponseEntity<?> createTravelLogs(
+            @RequestParam Long tripId
+            , @RequestPart(name = "data") @Valid TravelLogRequestDto requestDto
+            , @RequestPart(name = "files") List<MultipartFile> files
+            , @AuthenticationPrincipal String username
+    ) {
+        log.info("여행 일지 생성 API 호출 - 사용자 {}, 여행ID: {}", username, tripId);
+
+        travelLogService.createTravelLog(requestDto, tripId, username, files);
+
+        return ResponseEntity.ok()
+                .body(ApiResponse.success("여행일지가 성공적으로 생성되었습니다", null));
+    }
+}
